0f8ef955c91d220f3a1511a9841460dea227a5b5
JoaquinPerrier <73315138+JoaquinPerrier@users.noreply.github.com>
2023-01-19 16:59:47 -0300

    Quitando imports no usados

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-import NavBar from './components/NavBar'
-import ItemListConteiner from './components/itemListConteiner'
+import "./App.css";
+import NavBar from "./components/NavBar";
+import ItemListConteiner from "./components/itemListConteiner";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
       <NavBar />
-      <ItemListConteiner greeting={"Bienvenido! Eija un producto"}/>
+      <ItemListConteiner greeting={"Bienvenido! Eija un producto"} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
