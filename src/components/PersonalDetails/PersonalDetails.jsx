6e02651cc985e659edad097b4d08b546e64fb16d
JoaquinPerrier <73315138+JoaquinPerrier@users.noreply.github.com>
2023-02-12 22:13:08 -0300

    Mostrando productos en el /cart

--- src/components/PersonalDetails/PersonalDetails.jsx
+++ src/components/PersonalDetails/PersonalDetails.jsx
@@ -3,34 +3,35 @@ import styles from "./personalDetails.module.css";
 import { Link } from "react-router-dom";
 
 const PersonalDetails = ({ data }) => {
+  console.log(data.productos);
   return (
     <div className={styles.conteiner}>
-      <div className="col">
+      <div className={`col ${styles.avatar}`}>
         <img src={data.avatar} className="card-img-top" alt="..." />
       </div>
       <div className="col">
-        <div>
+        <div className={`col ${styles.personales}`}>
           <u>
             <h3>Datos personales:</h3>
           </u>
-          <h5>
+          <h4>
             <u>Nombre y Apellido:</u> {data.nombre}
             {data.apellido}
-          </h5>
-          <h5>
+          </h4>
+          <h4>
             <u>DNI:</u> {data.dni}
-          </h5>
-          <h5>
+          </h4>
+          <h4>
             <u>Domicilio:</u> {data.domicilio}
-          </h5>
-          <h5>
+          </h4>
+          <h4>
             <u>Ciudad:</u> {data.ciudad}
-          </h5>
-          <h5>
+          </h4>
+          <h4>
             <u>Pais:</u> {data.pais}
-          </h5>
+          </h4>
         </div>
-        <div>
+        <div className={styles.prod}>
           <u>
             <h3>Productos:</h3>
           </u>
@@ -41,8 +42,25 @@ const PersonalDetails = ({ data }) => {
               de productos
             </h5>
           ) : (
-            <h5>{data.productos}</h5>
+            data.productos.map((el) => {
+              return (
+                <div className={`container text-center ${styles.prod}`}>
+                  <div className="row">
+                    <div className="col">
+                      <h5>{el.title}</h5>
+                    </div>
+                    <div className="col">
+                      <img src={el.image} className="card-img-top" alt="..." />
+                    </div>
+                    <div className="col">
+                      <h5>$ {el.price}</h5>
+                    </div>
+                  </div>
+                </div>
+              );
+            })
           )}
+          <button class="btn btn-success">Finalizar compra!</button>
         </div>
       </div>
     </div>
